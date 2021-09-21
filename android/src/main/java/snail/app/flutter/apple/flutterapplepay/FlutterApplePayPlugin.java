291b0fb6afc64b939598fcf82b1370368ea72200
helen@hktaxiapp.com/ <helen@hktaxiapp.com>
2021-09-21 12:11:28 +0800

    Initial push

--- android/src/main/java/snail/app/flutter/apple/flutterapplepay/FlutterApplePayPlugin.java
+++ android/src/main/java/snail/app/flutter/apple/flutterapplepay/FlutterApplePayPlugin.java
@@ -7,13 +7,35 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterApplePayPlugin */
-public class FlutterApplePayPlugin implements MethodCallHandler {
+public class FlutterApplePayPlugin implements MethodCallHandler, FlutterPlugin {
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_apple_pay");
-    channel.setMethodCallHandler(new FlutterApplePayPlugin());
-  }
+  private MethodChannel methodChannel;
 
+  public static void registerWith(Registrar registrar) {
+    FlutterApplePayPlugin instance = new FlutterApplePayPlugin();
+    instance.initInstance(registrar.messenger(), registrar.context());
+  }
+  
+  public void initInstance(BinaryMessenger messenger, Context context){
+    methodChannel = new MethodChannel(messenger, "flutter_apple_pay");
+    methodChannel.setMethodCallHandler(new FlutterApplePayPlugin());
+  }
+  
+  private void dispose(){
+    methodChannel.setMethodCallHandler(null);
+    methodChannel = null;
+  }
+  
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding){
+    initInstance(binding.getBinaryMessenger(), binding.getApplicationContext());
+  }
+  
+  @Override
+  public void onDetachedFromEngine(FlutterPlugin binding){
+    dispose();
+  }
+  
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("getPlatformVersion")) {
--- example/android/app/src/main/java/snail/app/flutter/apple/flutterapplepayexample/MainActivity.java
+++ example/android/app/src/main/java/snail/app/flutter/apple/flutterapplepayexample/MainActivity.java
@@ -1,13 +1,14 @@
 package snail.app.flutter.apple.flutterapplepayexample;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+// import android.os.Bundle;
+// import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+// import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+// public class MainActivity extends FlutterActivity {
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(savedInstanceState);
+//     GeneratedPluginRegistrant.registerWith(this);
+//   }
+// }
